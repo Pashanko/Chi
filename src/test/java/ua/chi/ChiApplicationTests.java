beb37fefceb36710ddf9c8433f8da9e44bc110df
Pashanko <pnenchyn@gmail.com>
2023-07-06 20:22:13 +0300

    Initial commit

---
+++ src/test/java/ua/chi/ChiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.chi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
